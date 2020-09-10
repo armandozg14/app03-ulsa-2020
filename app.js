5c31d45718785cb985dea811bcc2cd69509030f2
Jesus Armando Zamora Gamez <armando.zamora14@hotmail.com>
2020-09-09 17:36:56 -0700

    Rick y Morty

--- app.js
+++ app.js
@@ -1,50 +1,114 @@
 const baseUrl = 'https://rickandmortyapi.com/api/';
 const characterList = document.getElementById('characters-list');
-const locationsList = document.getElementById('locations-list');
+const locationList = document.getElementById('location-name');
+const episodesList = document.getElementById('episode-info');
+
+// const locationList = {
+//     name: document.getElementById('location-name'),
+//     type: document.getElementById('location-type'),
+//     dimension: document.getElementById('location-dimension')
+// }
+// const episodesList = {
+//     episode: document.getElementById('episode-num'),
+//     name: document.getElementById('episode-name'),
+//     air_date: document.getElementById('episode-date'),
+//     characters: document.getElementById('episode-characters')
+// }
 
 const GetCharacterList = async url =>{
+    // fetch(`${baseUrl}${url}`).then(res => console.log(res.json()));
 
-    const resp= await fetch(`${baseUrl}${url}`);
-    const data = await resp.json();
+    //Puede ser de las dos formas, ya sea con fetch o con async
+
+    const response = await fetch(`${baseUrl}${url}`);
+    const data = await response.json();
     const {results} = data;
-    
-    console.log(resp);
-
-    const infoArr = results.map(element => {
-        const {image,url} = element;
-        return {characterImage:image, characterUrl: url};
+    //console.log(await data.results);
+    const infoArray = results.map(element =>{ //map devuelve arreglos
+        //console.log(element);
+        const{image, url}= element;
+        return {characterImg: image, characterUrl: url};
+        //console.log(image);
     });
 
-    await infoArr.forEach(element => {
+    await infoArray.forEach(element => { //forEach recorre arreglos
         const imgElement = document.createElement('img');
-        imgElement.src = element.characterImage;
-        imgElement.onclick = ()=>{
-            localStorage.setItem('characterUrl',element.characterUrl);
-            window.location.href="file:///F:/Documents/programacion-hipermedia/app03-ulsa-2020/character.htm";
+        imgElement.src = element.characterImg;
+        imgElement.onclick = ()=> {
+            localStorage.setItem('characterUrl', element.characterUrl);
+            window.location.href='file:///D:/Documentos/programacion-hipermedia/app03/character.htm';
         };
         characterList.appendChild(imgElement);
     });
 
+
+    //console.log(await imgArray);
 }
 
-GetLocationsList = async url =>{
-    const data = await fetch(`${baseUrl}${url}`);
-    const dataJson = await data.json();
-    const results = dataJson.results;
+const GetLocation = async url =>{
+    const res = await fetch(`${baseUrl}${url}`);
+    const data = await res.json();
+    const {results} = data;
+    //console.log(await data.results);
 
-    console.log(dataJson);
-    const locationResults = results.map(element =>{
-        const {name, dimension} = element;
-        return {locationName:name, locationDimension:dimension};
-        
-    })
-    locationResults.forEach(element => {
-        locationsList.innerHTML += `<div id="list-Style"> <li>${element.locationName}</li>
-        <li>${element.locationDimension}</li> <br> </div>` 
+    const locationArray = results.map(location =>{
+         //console.log(location);
+       
+         const{name, type, dimension} = location;
+         //locationList.name.innerHTML = name;
+         //locationList.type.innerHTML = type;
+         //locationList.dimension.innerHTML = dimension;
+         return{ locationName: name, locationType: type, locationDimension: dimension};
+      });
+
+
+        await locationArray.forEach(element => {
+            //console.log(element);
+
+            locationList.innerHTML += 
+            `<li>
+                 Name: ${element.locationName}
+                 <ul>
+                        <div>-Location: ${element.locationType}</div>
+                        <div>-Dimension: ${element.locationDimension}</div>
+                 </ul>
+            </li>`
+
+            // locationList.name.innerHTML += `<li>${element.locationName}</li>`;
+            // locationList.type.innerHTML += `<li>${element.locationType}</li>`;
+            // locationList.dimension.innerHTML += `<li>${element.locationDimension}</li>`;
+         });
+}
+
+const GetEpisodesList = async url =>{
+    const res = await fetch(`${baseUrl}${url}`);
+    const data = await res.json();
+    const {results} = data;
+    //console.log(await data.results);
+
+    const episodeArray = results.map(ep =>{
+        console.log(ep);
+        const{episode, name, air_date, characters} = ep;
+        return{episodeNum: episode, episodeName : name, episodeDate: air_date, episodeCharacters: characters};
     });
-    ;
 
-}
-GetLocationsList('location');
+    //const episodeCharactersArray = results.map
+
+
+    await episodeArray.forEach(element =>{
+
+        episodesList.innerHTML +=
+        `<tr>
+                <td>${element.episodeNum}</td>
+                <td> ${element.episodeName}</td>
+                <td>${element.episodeDate}</td>
+        </tr>`;
+         //<td>-Characters: ${element.episodeCharacters}</td>
+
+
+    });
+};
 
 GetCharacterList('character');
+GetLocation('location');
+GetEpisodesList('episode');
